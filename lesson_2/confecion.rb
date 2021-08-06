e9e38a2eecaac9016df1ab1c5d0a088fc31e986a
jerald <btljrl22@gmail.com>
2021-08-06 21:36:57 +0800

    refactor confecion

--- lesson_2/confecion.rb
+++ lesson_2/confecion.rb
@@ -1,29 +1,23 @@
 class Confection
+
+  def initialize
+    @bake = bake
+  end
+
   def bake
-    "Set baking at 350 degree for 25 munites"
+    puts "Set baking at 350 degree for 25 munites"
   end
 end
 
 class CupCakes < Confection
-  def frost
-    "Applying frosting"
+  def bake
+    super
+    puts "Applying frosting"
   end
-
-  def prepare
-    puts bake
-    puts frost
-  end
-
 end
 
 class BananaCakes < Confection
-  def prepare
-    puts bake
-  end
 end
 
 cup_cakes = CupCakes.new()
-cup_cakes.prepare
-
 banana_cakes = BananaCakes.new()
-banana_cakes.prepare
