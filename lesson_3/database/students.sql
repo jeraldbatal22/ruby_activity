cba9c863e070156884a1db7d947ba78b989c390b
jerald <btljrl22@gmail.com>
2021-08-10 20:20:19 +0800

    database sql part 2 activity

--- lesson_3/database/students.sql
+++ lesson_3/database/students.sql
@@ -1,15 +1,28 @@
-CREATE TABLE "CREATE TABLE students (id integer PRIMARY KEY, first_name text, middle_name text, last_name text, age integer, location text, created_at timestamp without time zone NOT NULL, updated_at timestamp without time zone NOT NULL);"
+"CREATE STUDENTS TABLE"
+CREATE TABLE students (id integer PRIMARY KEY, first_name CHARACTER VARYING(255) NOT NULL, middle_name CHARACTER VARYING(255) NOT NULL, last_name CHARACTER VARYING(255) NOT NULL, age NUMERIC(8), location text);
 
-INSERT 6 RECORDS OF STUDENTS
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (1, 'Juan', 'Dela', 'Cruz', 20, 'Manila')
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (2, 'Anne', 'Dela', 'Wall', 20, 'Surigao')
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (3, 'Theresa', 'Dela', 'Joseph', 20, 'Quezon City')
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (4, 'Issac', 'Dela', 'Gray', 20, 'Caloocan City')
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (5, 'Zack', 'Dela', 'Matthews', 20, 'Bulacan')
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (6, 'Finn', 'Dela', 'Lam', 20, 'Tarlac')
+"INSERT 6 RECORDS OF STUDENTS"
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (1, 'Juan', 'Dela', 'Cruz', 20, 'Manila');
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (2, 'Anne', 'Dela', 'Wall', 25, 'Surigao');
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (3, 'Theresa', 'Dela', 'Joseph', 23, 'Quezon City');
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (4, 'Issac', 'Dela', 'Gray', 21, 'Caloocan City');
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (5, 'Zack', 'Dela', 'Matthews', 18, 'Bulacan');
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES (6, 'Finn', 'Dela', 'Lam', 26, 'Tarlac');
 
-UPDATE FIRST RECORD
+"UPDATE FIRST RECORD"
 UPDATE students SET first_name = 'JuanUpdated', middle_name = 'DelaUpdated', last_name = 'CruzUpdated', age = 24, location = 'Neptune' WHERE id = 1;
 
-DELETE LAST RECORD
+"DELETE LAST RECORD"
 DELETE FROM students WHERE id = 6
+
+"DISPLAY COUNT OF ALL USER"
+SELECT COUNT(id) from students
+
+"SELECT ALL STUDENTS WITH LOCATION IS MANILA"
+SELECT location FROM students WHERE location = 'Manila';
+
+"SELECT AVG(age) FROM students;"
+SELECT AVG(age) FROM students;
+
+"DISPLAY ALL STUDENTS BY AGE DESC"
+SELECT * FROM students ORDER BY age DESC;
